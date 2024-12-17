8dd461b12fc76947ff20ffd72a80fe023e65f8bd
josue fuentes <josue.chaqui@gmail.com>
2024-12-17 10:12:08 -0600

    🚧 Add local network CORS configuration for development environment

--- src/config/corsConfig.ts
+++ src/config/corsConfig.ts
@@ -20,11 +20,25 @@ const corsOptionsProduction: cors.CorsOptions = {
   }
 };
 
+const corsLocalNetwork: cors.CorsOptions = {
+  origin: (origin, callback) => {
+    if (!origin || /^http:\/\/localhost:\d+$/.test(origin) || /^http:\/\/192\.168\.\d+\.\d+:\d+$/.test(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  }
+};
+
 const getCorsOptions = (env: string): cors.CorsOptions => {
   if (env === "development") {
     return corsOptionsDevelopment;
   } else if (env === "production") {
     return corsOptionsProduction;
+  }
+  else if (env === "localnetwork") {
+    return corsLocalNetwork;
+    
   } else {
     return {}; // Default CORS options
   }
