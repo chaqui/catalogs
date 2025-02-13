9355a168f07dfa2b7067fd7154eba16e53d45a96
josue fuentes <josue.chaqui@gmail.com>
2025-02-13 10:03:52 -0600

    ✨ Add PostgreSQL support in Sequelize configuration and update package dependencies

--- src/storage/mysql/Main.ts
+++ src/storage/mysql/Main.ts
@@ -1,18 +1,35 @@
 import { Sequelize } from "@sequelize/core";
 import { MariaDbDialect } from "@sequelize/mariadb";
+import { PostgresDialect } from "@sequelize/postgres";
 import * as dotenv from "dotenv";
 import Catalog from "../../models/sequelize/Catalog.model";
 import Item from "../../models/sequelize/Item.model";
 
 dotenv.config();
 
-const sequelize = new Sequelize({
-      dialect: MariaDbDialect,
-      host: process.env.MYSQL_HOST || "localhost",
-      port: parseInt(process.env.MYSQL_PORT || "3306"),
-      user: process.env.MYSQL_USER || "root",
-      password: process.env.MYSQL_PASSWORD || "password",
-      database: process.env.MYSQL_DATABASE || "catalogs",
-      models: [Catalog, Item],
-    }); 
+const database = process.env.DB || "mysql";
+let sequelize;
+if (database === "postgres") {
+  sequelize = new Sequelize({
+    dialect: PostgresDialect,
+    host: process.env.PG_HOST || "localhost",
+    port: parseInt(process.env.PG_PORT || "5432"),
+    user: process.env.PG_USER || "root",
+    password: process.env.PG_PASSWORD || "password",
+    database: process.env.PG_DATABASE || "catalogs",
+    ssl: true,
+    clientMinMessages: "notice",
+    models: [Catalog, Item],
+  });
+} else {
+  sequelize = new Sequelize({
+    dialect: MariaDbDialect,
+    host: process.env.MYSQL_HOST || "localhost",
+    port: parseInt(process.env.MYSQL_PORT || "3306"),
+    user: process.env.MYSQL_USER || "root",
+    password: process.env.MYSQL_PASSWORD || "password",
+    database: process.env.MYSQL_DATABASE || "catalogs",
+    models: [Catalog, Item],
+  });
+}
 export default sequelize;
